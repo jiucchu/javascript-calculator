3fb14a003aab70a810f025b9d4e7fd000327efa7
jiucchu <jiucchu@gmail.com>
2024-12-09 21:33:37 +0900

    feat: 결과값 출력 구현

--- src/utils/IOutils.js
+++ src/utils/IOutils.js
@@ -1,8 +1,13 @@
 import { Console } from "@woowacourse/mission-utils";
 
 export class InputUtils {
-
     static async inputString() {
         return await Console.readLineAsync('덧셈할 문자열을 입력해주세요.\n');
+    }
+}
+
+export class OutputUtils {
+    static printSum(sum) {
+        Console.print('값 :' + sum);
     }
 }
