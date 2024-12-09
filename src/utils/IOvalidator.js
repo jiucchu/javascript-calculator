0f785f30c33e68bf2f06c6e6733663c2363a6cdd
jiucchu <jiucchu@gmail.com>
2024-12-09 21:31:58 +0900

    feat: input validator 구현

--- src/utils/IOvalidator.js
+++ src/utils/IOvalidator.js
@@ -1,4 +1,13 @@
+import { ERROR_MESSAGE } from "./Constants";
+
 export default class IOvalidator {
+    static InputIsEmpty(input) {
+        if ((input === null) || (input === '')) {
+            return true
+        }
+
+        return false
+    }
 
     static checkCustomSeporator(inputString) {
         if (inputString.startWith('//')) {
@@ -6,11 +15,35 @@ export default class IOvalidator {
             customDelimeterEndIndex = inputString.indexof('\\n') + 2;
 
             if (customDelimeterStartIndex == -1) {
-                throw new Error('[ERROR]');
+                throw new Error(ERROR_MESSAGE.CUSTOM_DELIMETER_ERROR);
             }
 
             return inputString.subString(customDelimeterStartIndex, customDelimeterEndIndex + 1);
         }
+    }
+
+    static splitString(string, delimeter) {
+        if (delimeter == null) {
+            delimeter = (/:|,/);
+        }
+
+        return string.split(delimeter);
+    }
+
+    static stringToNumber(stringNumbers) {
+        stringNumbers.map(number => {
+            number = Number(string);
+
+            if (Number.isNaN(string)) {
+                throw new Error(ERROR_MESSAGE.INPUT_IS_NOT_NUMBER);
+            }
+
+            if (number <= 0) {
+                throw new Error(ERROR_MESSAGE.INPUT_IN_NOT_POSITIVE);
+            }
+        })
+
+        return stringNumbers;
     }
 
 }
